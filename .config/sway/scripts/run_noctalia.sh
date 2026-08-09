7181faa1065ba70c2d01c3c9b115bfcba5dd051b
ezequielgk <rox737280@gmail.com>
2026-08-09 01:18:03 -0300

    chore: clear legacy content from main (preserved in legacy branch)

--- .config/sway/scripts/run_noctalia.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-killall noctalia 2>/dev/null
-
-sleep 0.5
-
-noctalia &
--- .config/sway/scripts/swayidle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-killall swayidle 2>/dev/null
-
-swayidle -w \
-    timeout 1800 'systemctl suspend'
